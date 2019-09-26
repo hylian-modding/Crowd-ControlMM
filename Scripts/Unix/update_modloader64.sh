8a6d09d8e2b069ce22d76c1c5002ede51053d458
spiceywolf <spiceywolf.archaicsoft@outlook.com>
2019-09-26 12:00:53 -0400

    Fixed update script to add ModLoader64 repo if missing.

--- Scripts/Unix/update_modloader64.sh
+++ Scripts/Unix/update_modloader64.sh
@@ -2,6 +2,7 @@
 cd ../../
 
 # Update the ModLoader64 SubModule
+git submodule add https://github.com/hylian-modding/ModLoader64 ModLoader64
 git submodule update --init --recursive
 git submodule foreach --recursive git fetch
 git submodule foreach git merge origin master
